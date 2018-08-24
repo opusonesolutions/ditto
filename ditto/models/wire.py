9fc2bcc953e44c23d1b2e35781a4ded74fe68829
ngensoll <nicolas.gensollen@nrel.gov>
2018-08-24 11:19:12 -0600

    Add unit information in the models

--- ditto/models/wire.py
+++ ditto/models/wire.py
@@ -5,65 +5,90 @@ from .base import DiTToHasTraits, Float, Unicode, Any, Int, List, observe, Insta
 
 from .position import Position
 
+from ..constant import M, CM, MM, A, OHM
+
 
 class Wire(DiTToHasTraits):
     phase = Unicode(
-        help="""The phase (A, B, C, N, s1, s2) of the wire""", default_value=None
+        help="""The phase (A, B, C, N, s1, s2) of the wire""",
+        default_value=None,
+        unit=None,
     )
     nameclass = Unicode(
-        help="""The nameclass (e.g. 1/0_ACSR) of the wire""", default_value=None
+        help="""The nameclass (e.g. 1/0_ACSR) of the wire""",
+        default_value=None,
+        unit=None,
     )
     X = Float(
         help="""The horizontal placement of the wire on a cross section of the line w.r.t. some point of reference (typically one wire on the configuration)""",
         default_value=None,
+        unit=M,
     )
     Y = Float(
         help="""The vertical placement above (or below) ground of the wire on a cross section of the line w.r.t. some point of reference (typically one wire on the configuration)""",
         default_value=None,
+        unit=M,
     )
-    diameter = Float(help="""The diameter of the conductor""", default_value=None)
-    gmr = Float(help="""The geometric mean radius of the wire""", default_value=None)
+    diameter = Float(
+        help="""The diameter of the conductor""", default_value=None, unit=MM
+    )
+    gmr = Float(
+        help="""The geometric mean radius of the wire""", default_value=None, unit=CM
+    )
     ampacity = Float(
         help="""The ampacity rating for the wire under nomal conditions""",
         default_value=None,
+        unit=A,
     )
     emergency_ampacity = Float(
         help="""The ampacity rating for the wire under emergency conditions""",
         default_value=None,
+        unit=A,
     )
-    resistance = Float(help="""The total resistance of the wire""", default_value=None)
+    resistance = Float(
+        help="""The total resistance of the wire""", default_value=None, unit=OHM
+    )  # I'm putting ohm for consistency but we have to change that to be per unit length
     insulation_thickness = Float(
         help="""Thickness of the insulation around the secondary live conductors""",
         default=None,
+        unit=MM,
     )
     is_fuse = Int(
         help="""This flag indicates whether or not this wire is also a fuse""",
         default_value=None,
+        unit=None,
     )
     is_switch = Int(
         help="""This flag indicates whether or not this wire is also a switch""",
         default_value=None,
+        unit=None,
     )
     is_open = Int(
         help="""This flag indicates whether or not the line is open (if it is a switch/fuse/breaker/recloser/sectionalizer/network protector).""",
         default_value=None,
+        unit=None,
     )
     # Modification: Nicolas Gensollen (June 2018)
     # fuse_limit --> interrupting_rating (more generic)
     interrupting_rating = Float(
         help="""The maximum current that can pass through the wire before the equipment disconnects.""",
         default_value=None,
+        unit=A,
     )
     concentric_neutral_gmr = Float(
         help="""The geometric mean radius of the neutral for a concentric wire""",
         default_value=None,
+        unit=CM,
     )
     concentric_neutral_resistance = Float(
         help="""The total resistance of the neutral for a concentric wire""",
         default_value=None,
+        unit=OHM,  # Again, putting ohm but have to change to be per unit
     )
     concentric_neutral_diameter = Float(
-        help="""The diameter of the neutral for a concentric wire""", default_value=None
+        help="""The diameter of the neutral for a concentric wire""",
+        default_value=None,
+        unit=MM,
     )
 
     # Modification: Nicolas Gensollen (December 2017)
@@ -72,6 +97,7 @@ class Wire(DiTToHasTraits):
     drop = Int(
         help="""Set to 1 if the object should be dropped in the writing process. Otherwise leave 0.""",
         default_value=0,
+        unit=None,
     )
 
     # Modification: Nicolas (December 2017)
@@ -79,24 +105,28 @@ class Wire(DiTToHasTraits):
     is_recloser = Int(
         help="""This flag indicates whether or not this wire is also a recloser""",
         default_value=None,
+        unit=None,
     )
 
     # Modification: Nicolas (January 2018)
     is_breaker = Int(
         help="""This flag indicates whether or not this wire is also a recloser""",
         default_value=None,
+        unit=None,
     )
 
     # Modification: Nicolas (June 2018)
     is_network_protector = Int(
         help="""This flag indicates whether or not this wire is also a network protector.""",
         default_value=None,
+        unit=None,
     )
 
     # Modification: Nicolas (August 2018)
     is_sectionalizer = Int(
         help="""This flag indicates whether or not this wire is also a sectionalizer.""",
         default_value=None,
+        unit=None,
     )
 
     def build(self, model):
